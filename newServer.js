fcb5a2e15ea9ddc01747d7cf662fec5104bedb7c
elias-da <99768891+elias-da@users.noreply.github.com>
2023-03-26 21:46:11 +0200

    Add minor changes

--- newServer.js
+++ newServer.js
@@ -10,11 +10,21 @@ app.use(express.json())
 var currentKey = ""
 var currentPassword = ""
 
+function authenticateToken(req,rest,next){
+    if(currentKey == ""){
+        res.redirect("/identify")
+    } else if(jwt.verify(currentKey, process.env.ACCESS_TOKEN_SECRET)){
+        next()
+    } else{
+        res.redirect("/identify")
+    }
+}
+
 app.get('/', (req, res) => {
     const username = req.body.password
     const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
     currentKey = token
-    currentPassword = username
+    currentPassword = username      // remember to hash password
     res.redirect("/granted")
 })
 
@@ -22,7 +32,7 @@ app.get('/identify', (req, res) => {
     res.render('identify.ejs')
 })
 
-app.get('/granted', (req, res) => {
+app.get('/granted', authenticateToken, (req, res) => {
     res.render('start.ejs')
 })
 
